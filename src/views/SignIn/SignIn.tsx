c1c3f3d7bb27d80da85af074ce6c146b52b26227
Yixian Zhang <Yixianzhangivian@Gmail.com>
2022-09-05 02:25:39 -0400

    user login via SignIn page

--- src/views/SignIn/SignIn.tsx
+++ src/views/SignIn/SignIn.tsx
@@ -1,9 +1,31 @@
-// import { useState } from "react";
+import React, { useState } from "react";
 import styles from "./SignIn.module.css";
 import img from "../../assets/illustration/Illustration_Home_SignOn.png";
 import GoogleIcon from "../../assets/icons/Google.png";
+import { useNavigate } from "react-router-dom";
+import { auth } from "../../config/firebase"
+import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
 
-export function SignIn() {
+export interface ILoginPageProps {}
+
+const LoginPage: React.FunctionComponent<ILoginPageProps> = (props) => {
+  const auth = getAuth();
+  const navigate = useNavigate();
+  const [authing, setAuth] = useState(false);
+
+  const signInWithGoogle = async () => {
+      setAuth(true);
+
+      signInWithPopup(auth, new GoogleAuthProvider())
+          .then((response) => {
+              console.log(response.user.uid);
+              navigate('/'); // going back to homepage
+          })
+          .catch((error) => {
+              console.log(error);
+              setAuth(false);
+          });
+  };
 
   return (
     <div className={styles.container}>
@@ -17,15 +39,19 @@ export function SignIn() {
           Welcome to <span className={styles.blue}>FIFO</span>
         </div>
         <div className={styles.BodyBold}>Get your questions answered</div>
+        {/* TODO merge the button and href into functional links */}
+        <button className="signin-with-google-btn" 
+        onClick={() => signInWithGoogle()} disabled={authing}>Continue with Google</button>
         <a className={styles.btn} href="/user/enrollment">
           <img
             className={styles.icon}
             alt={"Google Icon"}
             src={GoogleIcon}
           ></img>
-          <div>Continue with Google</div>
         </a>
+        
       </div>
     </div>
   );
 }
+export default LoginPage;
